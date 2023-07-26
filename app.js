a239a67d6a493a78b7a0c5a49c94545681d9ace2
caoduong8x <caoduong.it8x@gmail.com>
2023-07-26 10:26:00 +0700

    Khoi tao server

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Welcome to http://toidicode.com");
+let http = require("http");
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<html>");
+    res.write("<head>");
+    res.write("<title>Node JS</title>");
+    res.write("</head>");
+    res.write("<body><h1>Node Server is running</h1></body>");
+    res.write("</head>");
+    res.end();
+  })
+  .listen(8000);
