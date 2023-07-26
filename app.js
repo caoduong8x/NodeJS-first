90180444843acdf4368159f79c60a88af764ed59
caoduong8x <caoduong.it8x@gmail.com>
2023-07-26 10:46:09 +0700

    File System (fs) readFile, writeFile

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 let http = require("http");
+let fs = require("fs");
+
 http
   .createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("<html>");
-    res.write("<head>");
-    res.write("<title>Node JS</title>");
-    res.write("</head>");
-    res.write("<body><h1>Node Server is running</h1></body>");
-    res.write("</head>");
-    res.end();
+    fs.readFile("Code.html", "utf8", (err, data) => {
+      if (err) throw err;
+      res.write(data);
+      fs.writeFile("Code2.html", data, "utf8", (err) => {
+        if (err) throw err;
+      });
+      res.end();
+    });
   })
   .listen(8000);
