83fd4f1c9328e11c4c80215b14aa98ec0810e105
caoduong8x <caoduong.it8x@gmail.com>
2023-07-26 11:15:43 +0700

    fs and url

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 let http = require("http");
 let fs = require("fs");
+let url = require("url");
 
 http
   .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    fs.readFile("Code.html", "utf8", (err, data) => {
-      if (err) throw err;
-      res.write(data);
-      fs.writeFile("Code2.html", data, "utf8", (err) => {
+    let parse = url.parse(req.url, true);
+    let path = parse.path;
+    if (path === "/") {
+      fs.readFile("home.html", "utf8", (err, data) => {
         if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        res.end();
       });
-      res.end();
-    });
+    } else {
+      let load = path.slice(1) + ".html";
+      fs.readFile(load, "utf8", (err, data) => {
+        if (err) {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          res.write(load);
+          res.end();
+        } else {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.write(data);
+          res.end();
+        }
+      });
+    }
   })
   .listen(8000);
